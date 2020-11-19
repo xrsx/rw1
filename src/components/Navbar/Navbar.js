0fcb3e179c0f7cc985ba717838313d08555fc49a
xrsx <021novisad@gmail.com>
2020-11-19 19:35:16 +0100

    New commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,40 @@
-import React from 'react'
-import { Nav, NavbarContainer } from './Navbar.elements.js';
+import React, { useState } from 'react'
+import { IconContext } from 'react-icons/lib';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon, NavMenu, NavItem, NavLinks } from './Navbar.elements.js';
  
 const Navbar = () => {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
     return (
         <div>
-           <Nav>
-            <NavbarContainer>
-                
-            </NavbarContainer> 
-           </Nav> 
+          <IconContext.Provider value={{color: '#fff'}}>
+            <Nav>
+               <NavbarContainer>
+                    <NavLogo to='/'>
+                        <NavIcon/>
+                        Ultra
+                    </NavLogo>
+                    <MobileIcon onClick={handleClick}>
+                    {click ? <FaTimes /> : <FaBars />}</MobileIcon>
+                    <NavMenu onClick={handleClick} click={click}>
+                        <NavItem>
+                            <NavLinks to='/'>Home</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='services'>Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='products'>Products</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='products'>Products</NavLinks>
+                        </NavItem>
+                    </NavMenu> 
+                </NavbarContainer> 
+            </Nav>
+          </IconContext.Provider>  
         </div>
     )
 }
