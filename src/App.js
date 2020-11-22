e952663c1b8ee5b9e3900c514df3e24ba96a3e49
xrsx <021novisad@gmail.com>
2020-11-22 17:41:42 +0100

    New commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import GlobalStyle from './globalStyles'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Navbar } from './components';
-
+import React from "react";
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './pages/HomePage/Home';
+import { Navbar } from "./components";
 
 function App() {
   return (
     <Router>
-      <GlobalStyle/>
-      <Navbar/>
+      <GlobalStyle />
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+      </Switch>
     </Router>
   );
 }
 
 export default App;
- 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,42 +1,88 @@
-import React, { useState } from 'react'
-import { IconContext } from 'react-icons/lib';
+import React, { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon, NavMenu, NavItem, NavLinks } from './Navbar.elements.js';
- 
-const Navbar = () => {
-    const [click, setClick] = useState(false)
+import { IconContext } from 'react-icons/lib';
+import { Button } from '../../globalStyles';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    NavIcon,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavItemBtn,
+    NavLinks,
+    NavBtnLink
+} from './Navbar.elements';
 
-    const handleClick = () => setClick(!click)
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
     return (
-        <div>
-          <IconContext.Provider value={{color: '#fff'}}>
-            <Nav>
-               <NavbarContainer>
-                    <NavLogo to='/'>
-                        <NavIcon/>
-                        Ultra
-                    </NavLogo>
-                    <MobileIcon onClick={handleClick}>
-                    {click ? <FaTimes /> : <FaBars />}</MobileIcon>
-                    <NavMenu onClick={handleClick} click={click}>
-                        <NavItem>
-                            <NavLinks to='/'>Home</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to='services'>Services</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to='products'>Products</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to='products'>Products</NavLinks>
-                        </NavItem>
-                    </NavMenu> 
-                </NavbarContainer> 
-            </Nav>
-          </IconContext.Provider>  
-        </div>
-    )
+        <>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <Nav>
+                    <NavbarContainer>
+                        <NavLogo to='/' onClick={closeMobileMenu}>
+                            <NavIcon />
+                            ULTRA
+                        </NavLogo>
+                        <MobileIcon onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </MobileIcon>
+                        <NavMenu onClick={handleClick} click={click}>
+                            <NavItem>
+                                <NavLinks to='/' onClick={closeMobileMenu}>
+                                    Home
+                                </NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/services' onClick={closeMobileMenu}>
+                                    Services
+                                </NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/products' onClick={closeMobileMenu}>
+                                    Products
+                                </NavLinks>
+                            </NavItem>
+                            <NavItemBtn>
+                                {button ? (
+                                    <NavBtnLink to='/sign-up'>
+                                        <Button primary>SIGN UP</Button>
+                                    </NavBtnLink>
+                                ) : (
+                                        <NavBtnLink to='/sign-up'>
+                                            <Button onClick={closeMobileMenu} fontBig primary>
+                                                SIGN UP
+                                            </Button>
+                                        </NavBtnLink>
+                                    )}
+                            </NavItemBtn>
+                        </NavMenu>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
+        </>
+    );
 }
 
-export default Navbar
+export default Navbar;
